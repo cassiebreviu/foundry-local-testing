4143300f413301f25b3681a45c81ff7607e4e1c3
Cassie Breviu <cassieb@microsoft.com>
2025-10-07 16:37:21 -0500

    Remove deprecated files and update Foundry Local integration with new implementations for improved functionality and structure.

--- python/foundry-local-with-agf.py
+++ python/foundry-local-with-agf.py
@@ -1,47 +1,33 @@
 import asyncio
-from agent_framework.azure import AzureOpenAIChatClient
 from foundry_local import FoundryLocalManager
+from agent_framework import ChatAgent
+from agent_framework.openai import OpenAIChatClient
 
-### IMPORTANT NOTE: THIS DOES NOT CURRENTLY WORK ###
-# The AzureOpenAIChatClient does not currently support custom endpoints
-# This results in a connection error
 
 async def main():
-    
-
-    # By using an alias, the most suitable model will be downloaded
-    # to your end-user's device.
-    alias = "qwen2.5-0.5b"
 
     # Create a FoundryLocalManager instance. This will start the Foundry
-    
-    manager = FoundryLocalManager(alias)
+    manager = FoundryLocalManager()
+    modelInfo = manager.load_model("qwen2.5-0.5b")
+    print(f"Model info: {modelInfo}")
+
     service_url = manager.service_uri
+    service_url = service_url + "/v1"
     print(f"Foundry Local service URL: {service_url}")
+
     # check status of service
     manager.start_service()
     status = manager.is_service_running()
     print(f"Foundry Local service status: {status}")
-    # List available models
-    loaded = manager.list_loaded_models()
-    print(f"Models running in the service: {loaded}")
-    # Set the model to use and generate a streaming response
 
-    url = manager.endpoint + "/chat/completions"
-    deployment_name = manager.get_model_info(alias).id
-    print(f"Model info: {deployment_name}")
-    # The Foundry Local endpoint uses HTTP by default.
-    #update url to https
-    url = url.replace("http://", "https://")
-    print(f"Foundry Local endpoint: {url}")
-
-    agent = AzureOpenAIChatClient(
-        endpoint=url,
-        deployment_name=deployment_name,
+    agent = ChatAgent(
+    chat_client=OpenAIChatClient(
+        model_id=modelInfo.id,
+        base_url=service_url,
         api_key=manager.api_key  # API key is not required for local usage
-    ).create_agent(
-        instructions="You are good at telling jokes.",
-        name="Joker"
+        ),
+    instructions="You are good at telling jokes.",
+    name="Joker"
     )
 
     result = await agent.run("Tell me a joke about a pirate.")
